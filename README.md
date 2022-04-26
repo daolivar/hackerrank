e615d39517bf0a8adbedd8400c2349e641566b54
David Olivares <daveolivares01@gmail.com>
2022-04-25 22:45:51 -0700

    Adds solution for array ds

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# hackerrank
+# Hackerrank
+- [Array DS](https://github.com/daolivar/hackerrank/blob/main/array-ds.go)
---
+++ array-ds.go
@@ -0,0 +1,26 @@
+package hackerrank
+
+func reverseArray(a []int32) []int32 {
+	j := len(a) - 1
+	for i := 0; i < len(a)/2; i++ {
+		temp := a[i]
+		a[i] = a[j]
+		a[j] = temp
+		j--
+	}
+	return a
+}
+
+// Alternative approach
+// func reverseArray(a []int32) []int32 {
+// 	i := 0
+// 	j := len(a) - 1
+// 	for i < j {
+// 		temp := a[i]
+// 		a[i] = a[j]
+// 		a[j] = temp
+// 		i++
+// 		j--
+// 	}
+// 	return a
+// }
